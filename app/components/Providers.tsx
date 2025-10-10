07111d9ff9d9603499424b661d1a5cb93ee96d99
Saad_Arfin <saad.arfin.ug21@nsut.ac.in>
2025-10-10 22:24:29 +0530

    Add FileUpload component and integrate ImageKit for file uploads

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -4,29 +4,16 @@ import { SessionProvider } from "next-auth/react";
 import { ImageKitProvider } from "@imagekit/next";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
-const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY!;
+
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const authenticator = async () => {
-    try {
-      const res = await fetch("/api/imagekit-auth");
-      if (!res.ok) throw new Error("Failed to authenticate");
-      return res.json();
-    } catch (error) {
-      console.error("ImageKit authentication error:", error);
-      throw error;
-    }
-  };
 
   return (
     <SessionProvider refetchInterval={5 * 60}>
-      <ImageKitProvider
-          publicKey={publicKey}
-          urlEndpoint={urlEndpoint}
-          authenticator={authenticator}
-        >
-          {children}
-        </ImageKitProvider>
+      <ImageKitProvider urlEndpoint={urlEndpoint}>
+        {children}
+      </ImageKitProvider>
     </SessionProvider>
   );
 }
+
